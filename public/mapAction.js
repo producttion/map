fbd3e05469bdd9724cd09fb3bf7e3532b7f968fe
id10bat <my_nutthapat-26_@hotmail.com>
2017-08-06 19:03:30 +0700

    checked update

--- public/mapAction.js
+++ public/mapAction.js
@@ -1,37 +1,72 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBiWyIcR3Jv5xgG5lCKhkTlt6d8MYHmNqs",
-    authDomain: "production-636ab.firebaseapp.com",
-    databaseURL: "https://production-636ab.firebaseio.com",
-    projectId: "production-636ab",
-    storageBucket: "production-636ab.appspot.com",
-    messagingSenderId: "87472810879"
+    apiKey: "AIzaSyBEQeGpK8vz53WgoXaeP_EHBCJ8c_MaFhc",
+    authDomain: "appjongtaladth.firebaseapp.com",
+    databaseURL: "https://appjongtaladth.firebaseio.com",
+    projectId: "appjongtaladth",
+    storageBucket: "",
+    messagingSenderId: "530710859012"
 };
 firebase.initializeApp(config);
+
 
 var dbRef = firebase.database().ref()
 
 
 
 var checkVal = 0
-
+var checkedToNum = 0;
 function clickCheck() {
-    $('.button').click(function() {
+    $('.button').click(function () {
         var set$Checked = $('input', this).is(":checked");
         console.log(set$Checked);
         var setChecked = $('input', this).attr("value");
         console.log(setChecked);
+        if (set$Checked == true) {
+            $('input', this).checked = true;
+            checkedToNum = 1;
+        } else if (set$Checked == false) {
+            checkedToNum = 0;
+        }
         // var setBound = $(this).text();
         // console.log(setBound);
         var setValue = $(this).attr("value");
         console.log(setValue);
-        dbRef.child(`usersdata/data${setValue}`)
+        dbRef.child(`usersdata/data/${setValue}`)
             .set({
                 setBound: setValue,
-                [setChecked]: set$Checked
+                check: checkedToNum
             });
+
     });
 }
+// var dbRefCheckStart = 1;
+// var dbRefCheckEnd = 150;
+
+// var endBox1 = setInterval(function () {
+//     if (dbRefCheckStart == dbRefCheckEnd) {
+//         clearInterval(endBox1);
+
+//     }
+//     var textCheck = "check" + dbRefCheckStart + "";
+//     var attendeesRef = firebase.database().ref("usersdata/data/" + dbRefCheckStart + "")
+//    var ttt = attendeesRef.orderByChild("usersdata").once('value', function (snapshot) {
+        
+//         if (snapshot.val().check == 1) {     
+//                     $("#check1").prop("checked", true);
+//                     $("#check2").prop("checked", true);
+//         }
+//         console.log(snapshot.key + " |check: " + snapshot.val().check + " |setBound: " + snapshot.val().setBound)
+//     });
+
+
+//     // console.log("element:" + dbRefCheckStart+ " textCheck " +textCheck)
+//     dbRefCheckStart = dbRefCheckStart + 1;
+// })
+
+
+
+
 
 
 // var lock = 150;
@@ -41,7 +76,7 @@ function clickCheck() {
 
 
 
-//     var numPerple = dbRef.ref(`usersdata/data${element}`);
+//     var numPerple = dbRef.ref(`usersdata/data/${element}`);
 //     var numPerples = numPerple.on('value', function(snapshot) {
 //         console.log("Data:" + snapshot.val());
 //         var snap = snapshot.val()
